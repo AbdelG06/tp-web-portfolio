0e5f04e2272736346a741019c5c2d6d0e5c9231c
Abdelkrim Garwaoui <gkakiwawi@icloud.com>
2026-05-12 12:51:04 +0100

    css & js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Salut le JavaScript marche !");
+
+const btnMessage = document.getElementById("btn-message");
+const message = document.getElementById("message");
+
+btnMessage.addEventListener("click",function() {
+    message.textContent = "Vous avez cliqué sur le bouton !";
+    message.style.color = "blue";
+});
